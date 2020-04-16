54bcaeba36c82f352b2c54141fa94b1fdba05d64
Henrique Gudaites <52537422+Gudaites@users.noreply.github.com>
2020-04-16 13:51:39 -0300

    desafio completo

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,79 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid type.');
+    }
+
+    if (value <= 0) {
+      throw new AppError('Value must be grater than 0.');
+    }
+
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const balance = await transactionRepository.getBalance();
+
+      if (balance.total - value < 0) {
+        throw new AppError('Not sufficient balance.');
+      }
+    }
+
+    const categoryExist = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let createCategory;
+
+    if (!categoryExist) {
+      createCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(createCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    if (createCategory) {
+      transaction.category_id = createCategory.id;
+    } else if (categoryExist) {
+      transaction.category_id = categoryExist.id;
+    }
+
+    await transactionsRepository.save(transaction);
+
+    delete transaction.category_id;
+    delete transaction.created_at;
+    delete transaction.updated_at;
+
+    return transaction;
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,40 @@
+import csv from 'csvtojson';
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+import uploadConfig from '../config/upload';
+
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  file: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ file }: Request): Promise<Transaction[]> {
+    // console.log(file);
+    const filePath = path.join(uploadConfig.directory, file);
+    const csvJson = await csv().fromFile(filePath);
+
+    const createTransactionService = new CreateTransactionService();
+
+    const transactions: Transaction[] = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const element of csvJson) {
+      const { title, type, value, category } = element;
+
+      // eslint-disable-next-line no-await-in-loop
+      const transaction = await createTransactionService.execute({
+        title,
+        type,
+        value: Number.parseFloat(value),
+        category,
+      });
+
+      transactions.push(transaction);
+    }
+    return transactions;
   }
 }
 
